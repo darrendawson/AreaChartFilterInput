c4c52ffe0d1275accafd418dae66cc0cf96c703a
bear <thedarrendawson@gmail.com>
2020-05-20 17:10:22 -0700

    User can drag and drop min/max filter boundaries on a recharts Area graph to visually determine filter points

--- src/App.js
+++ src/App.js
@@ -2,25 +2,67 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ChartComponent from './ChartComponent';
+
+// Constants and Input Data ----------------------------------------------------
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let data = [];
+let prevAmount = 1000;
+for (let i = 0; i < 100; i++) {
+  let newAmount = prevAmount + getRandomInt(-100, 100);
+  if (newAmount < 0) {
+    newAmount = 0;
+  }
+  data.push({
+    'value': i,
+    'results': newAmount
+  });
+  prevAmount = newAmount;
+}
+
+// =============================================================================
+// <App/>
+// =============================================================================
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      'min': 4,
+      'max': 90
+    }
+  }
+
+  updateFilterValue = (value, filterType) => {
+    if ((filterType === 'min') && (this.state.min !== value)) {
+      this.setState({'min': value});
+    } else if ((filterType == 'max') && (this.state.max !== value)) {
+      this.setState({'max': value});
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <ChartComponent
+          data={data} min={this.state.min} 
+          max={this.state.max}
+          updateFilter={this.updateFilterValue}
+          minKey='min'
+          maxKey='max'
+        />
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/ChartComponent.js
@@ -0,0 +1,123 @@
+import React from 'react';
+
+// Imports ---------------------------------------------------------------------
+
+import {
+  AreaChart, Area, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ReferenceLine, ResponsiveContainer
+} from 'recharts';
+
+// Constants -------------------------------------------------------------------
+
+const NO_FILTER = -1;
+const MIN_FILTER = 1;
+const MAX_FILTER = 2;
+
+
+// =============================================================================
+// <ChartComponent/>
+// =============================================================================
+
+class ChartComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      hoveredValue: -1,
+      selectedFilter: NO_FILTER
+    }
+  }
+
+  // the x-axis represents the value in question (# of viewers, # of followers, etc)
+  // the y-axis represents the number of streamers that have that value
+  extractValueFromChart = (numStreamers, value, props) => {
+    let newValue = props.payload.value;
+    if (this.state.hoveredValue !== newValue) {
+      this.setState({'hoveredValue': newValue})
+    }
+
+    // If the user is dragging a filter, update the value
+    if ((this.state.selectedFilter === MIN_FILTER) && (newValue !== this.props.min) && (newValue < this.props.max)) {
+      this.props.updateFilter(newValue, this.props.minKey);
+    } else if ((this.state.selectedFilter === MAX_FILTER) && (newValue !== this.props.max) && (newValue > this.props.min)) {
+      this.props.updateFilter(newValue, this.props.maxKey);
+    }
+  }
+
+  // takes props.data and breaks it into 3 lists (filtered for being < min, filtered for being > max, and not filtered)
+  getFilteredData = (data) => {
+    let filtered = [];
+    for (let i = 0; i < data.length; i++) {
+      if (data[i]['value'] < this.props.min) {
+        filtered.push({'value': data[i]['value'], 'min-results': data[i]['results'], 'max-results': 0, 'results': 0 });
+      } else if (data[i]['value'] > this.props.max) {
+        filtered.push({'value': data[i]['value'], 'min-results': 0, 'max-results': data[i]['results'], 'results': 0 });
+      } else {
+        filtered.push({'value': data[i]['value'], 'min-results': 0, 'max-results': 0, 'results': data[i]['results'] });
+      }
+    }
+    return filtered;
+  }
+
+
+  // render --------------------------------------------------------------------
+
+  // lineType is either "Min" or "Max"
+  renderReferenceLines = (lineType) => {
+    let minLabel = "Min: " + this.props.min; // <- label of reference line
+    let maxLabel = "Max: " + this.props.max;
+    let minColor = "red";                    // <- the color of the reference line
+    let maxColor = "red";
+    let minWidth = 2;                        // <- the width of the reference line
+    let maxWidth = 2;
+    if (this.state.hoveredValue !== -1) {
+      let distanceToMin = Math.abs(this.props.min - this.state.hoveredValue);
+      let distanceToMax = Math.abs(this.props.max - this.state.hoveredValue);
+      if (distanceToMax > distanceToMin) {
+        minWidth = 5;
+      } else {
+        maxWidth = 5;
+      }
+    }
+    return {
+      'min': <ReferenceLine x={this.props.min} label={minLabel} stroke={minColor} strokeWidth={minWidth} style={{'cursor': 'ew-resize'}} onMouseDown={() => this.setState({'selectedFilter': MIN_FILTER})} />,
+      'max': <ReferenceLine x={this.props.max} label={maxLabel} stroke={maxColor} strokeWidth={maxWidth} style={{'cursor': 'ew-resize'}} onMouseDown={() => this.setState({'selectedFilter': MAX_FILTER})} />
+    }
+  }
+
+
+  render() {
+    let data = this.getFilteredData(this.props.data);
+    let referenceLines = this.renderReferenceLines();
+
+    return (
+      <div style={{'height': '100%', 'width': '100%'}}>
+        <p>Value: {this.state.hoveredValue}; Filter: {this.state.selectedFilter}</p>
+        <ResponsiveContainer width="80%" height="80%">
+
+          <AreaChart
+             data={data}
+             margin={{top: 20, right: 50, left: 20, bottom: 5,}}
+             onMouseLeave={() => this.setState({hoveredValue: -1, selectedFilter: NO_FILTER})}
+             onMouseUp={() => this.setState({selectedFilter: NO_FILTER})}
+           >
+
+             <CartesianGrid strokeDasharray="3 3" />
+             <XAxis dataKey="value" />
+             <YAxis />
+             <Tooltip formatter={this.extractValueFromChart}/>
+
+             <Area type="monotone" dataKey="results" stroke="#56c990" fill="#56c990" isAnimationActive={false}/>
+             <Area type="monotone" dataKey="min-results" stroke="grey" fill="grey" isAnimationActive={false}/>
+             <Area type="monotone" dataKey="max-results" stroke="grey" fill="grey" isAnimationActive={false}/>
+
+             {/* Make sure to render reference lines last so they get rendered above everything else*/}
+             {referenceLines['min']}
+             {referenceLines['max']}
+         </AreaChart>
+       </ResponsiveContainer>
+      </div>
+    )
+  }
+}
+
+
+export default ChartComponent;
